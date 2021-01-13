4f0d4d25f592e3dff621c7e63248f47a7436a5a7
Stavros <karatzas.stauros@gmail.com>
2021-01-13 13:51:24 +0100

    Added object repository

--- Program.cs
+++ Program.cs
@@ -9,21 +9,15 @@ namespace Task1._0._0
         {
 
             Repository.Clone("https://github.com/Skaratzas/repository", @"C:\GitHub\repository");
-
             Repository.Init(@"C:\GitHub\git");
 
+            Repository repository = new Repository(@"C:\GitHub\repository");
 
-            using (var repository = new Repository(@"C:\GitHub\repository")) {
-                var commits = repository.Commits;
-                foreach (var commit in commits)
-                {
-                    Console.WriteLine($"{commit.Id.ToString().Substring(0, 6)} " +
-                                      $"{commit.Author.When.ToLocalTime()} " +
-                                      $"{commit.MessageShort} " +
-                                      $"{commit.Author.Name}");
-                }
+            
 
-            }
+
+
+           
         }
     }
 }
