cf27be3530d9956da8418e6a672048fc898e1008
Stavros <karatzas.stauros@gmail.com>
2021-02-14 02:07:44 +0100

    parse the diff string using RegEx

--- NewCommit.cs
+++ NewCommit.cs
@@ -1,19 +1,21 @@
 using LibGit2Sharp;
 using System;
-
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Task1._0._1
 {
     class NewCommit
     {
-        static String id;
+        static string id;
         static DateTime dateTime;
         static string message;
         static string name;
-        Repository repository;
+        Repository repository;        
+        List<string> addedLines = new List<string>();
+        List<string> removedLines = new List<string>();
 
-
-        public NewCommit(String aId, DateTime aDateTime, string aMessage, string aName)
+        public NewCommit(string aId, DateTime aDateTime, string aMessage, string aName)
         {
             id = aId;
             dateTime = aDateTime;
@@ -37,16 +39,32 @@ namespace Task1._0._1
             {
                 if (previousCommit != null)
                 {
-                    var patch = repository.Diff.Compare<Patch>(previousCommit.Tree, commit.Tree);
+                    var patch = repository.Diff.Compare<Patch>(commit.Tree, previousCommit.Tree);
 
                     foreach (var pec in patch)
-                    {
+                    {                     
                         Console.WriteLine("{0} Lines changed: {1} = ({2}+ and {3}-)",
                             pec.Patch,
                             pec.LinesAdded + pec.LinesDeleted,
                             pec.LinesAdded,
                             pec.LinesDeleted);
-                    }
+
+
+                        string input = pec.Patch;
+
+                        string addedPattern = @"^\+\s.*$";
+                        foreach (Match match in Regex.Matches(input, addedPattern, RegexOptions.Multiline))
+                        {
+                            addedLines.Add(match.Value);
+                        }
+
+                        string removedPattern = @"^\-\s.*$";
+                        foreach (Match match in Regex.Matches(input, removedPattern, RegexOptions.Multiline))
+                        {
+                            removedLines.Add(match.Value);
+                        }
+                      
+                    }                   
                 }
 
                 id = commit.Id.ToString().Substring(0, 7);
@@ -56,8 +74,9 @@ namespace Task1._0._1
 
                 Console.WriteLine("\n" + id + " " + dateTime + " " + message + " " + name);
 
+
                 previousCommit = commit;
-            }
+            }         
 
         }
     }
